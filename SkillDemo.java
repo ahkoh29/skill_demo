7e23a8344c121ed3ea392c48c1a1269e6e4e8df8
amykoh29 <amykoh29@gmail.com>
2022-02-02 08:03:30 -0800

    imports and test that fails

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,10 @@
-
+public class SkillDemo{
+    public static int countInstanceOf(String word, String find){
+        int index=0, counter=0;
+        while(index < word.length() && word.indexOf(find, index)!=-1){
+            counter++;
+            index = word.indexOf(find, index) + find.length();
+        }
+        return counter;
+    }
+}
--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,1 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class SkillDemoTest{
+    @Test
+    public void countInstanceOfTest(){
+        assertEquals(3, SkillDemo.countInstanceOf("Happy Lunar New Year", "e")) //fails, should return 2
+    }
+}
