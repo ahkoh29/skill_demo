e7d45b160728605a7bb3269cd24bb9a9840e7b7c
amykoh29 <amykoh29@gmail.com>
2022-02-02 08:05:30 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest{
     @Test
     public void countInstanceOfTest(){
-        assertEquals(3, SkillDemo.countInstanceOf("Happy Lunar New Year", "e")) //fails, should return 2
+        assertEquals(3, SkillDemo.countInstanceOf("Happy Lunar New Year", "e")); //fails, should return 2
     }
 }
